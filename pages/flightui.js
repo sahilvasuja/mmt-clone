03d9c6307e86c152d77d7cf708bd1d5ff900a5ba
Sahil Vasuja <sahilvasuja@Sahil-Vasujas-MacBook-Pro.local>
2022-10-27 17:28:36 +0530

    ui is in progress

--- pages/flightui.js
+++ pages/flightui.js
@@ -12,7 +12,21 @@ const flightui = () => {
         <h2 className='text-black text-xl p-4'>ROUNDTRIP</h2>
         <h2 className='text-black text-xl p-4'>MULTICITY</h2>
     </div>
-
+    <div className="border border-black w-1/4 h-36 flex flex-col  text-black p-2 ">
+        <h5 className="text-xl">FROM</h5>
+        <input className="text-2xl bg-white text-black" value={journeyBeggin} onChange={Startjourney}></input>
+        <input className="text-lg bg-white" value={` ${journeyBeggin.substring(0,3)} ${journeyBeggin} International AIRPORT`}></input>
+      </div>
+      <div className="border border-black w-1/4 h-36 flex flex-col text-black p-2">
+        <h5 className="text-xl">TO</h5>
+        <input className="text-2xl bg-white" value={journeyOver} onChange={Endjourney} ></input>
+        <input className="text-lg bg-white" value={` ${journeyOver.substring(0,3)} ${journeyOver} International AIRPORT...`}></input>
+      </div>
+      <div className="border border-black w-1/5 h-36 flex flex-col  text-black p-2">
+        <div className="text-xl">DEPARTURE </div>
+        <input className="text-2xl bg-white" value={journeydate} onChange={date}></input>
+        <div className="text-lg">Friday</div>
+      </div>
    </>
   )
 }
