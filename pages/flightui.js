ab1ac7a2893b550f63a8843c6492329cea8ba5c9
Sahil Vasuja <sahilvasuja@Sahil-Vasujas-MacBook-Pro.local>
2022-10-31 11:40:36 +0530

    fetching data inprogress

--- pages/flightui.js
+++ pages/flightui.js
@@ -2,10 +2,17 @@ import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { supabase } from '../utils/supabase';
 import { FaArrowLeft,FaPlaneDeparture } from "react-icons/fa";
+import { useRouter } from 'next/router';
+
 const flightui = () => {
+    const Router=useRouter();
+    const [flightsearch,setFlightsearch]=useState(false);
     const [journeyBeggin,setJourneyBeggin]=useState("Delhi");
   const Startjourney=(e)=>{
       setJourneyBeggin(e.target.value);
+  }
+  const passengerdetail=(e)=>{
+    Router.push("/passenger");
   }
   const [journeyOver,setJourneyOver]=useState("Bengaluru");
   const Endjourney=(e)=>{
@@ -21,14 +28,50 @@ const flightui = () => {
       
       setMember(e.target.value + "Traveller");
   }
+
   useEffect(()=>{
     fetchData();
   },[]);
+
+  function handlesearch(e){
+    e.preventDefault();
+    setFlightsearch(true);
+  }
+//   async function getCurrentUser() {
+//     const {
+//       data: { session },
+//       error,
+//     } = await supabase.auth.getSession()
+
+//     if (error) {
+//       throw error
+//     }
+
+//     if (!session?.user) {
+//       throw new Error('User not logged in')
+//     }
+
+//     return session.user
+//   }
+
   async function fetchData(){
-    const { data, error } = await supabase
+    const { data: { user } } = await supabase.auth.getUser()
+    const { data, error,status} = await supabase
   .from('flight')
-  .select('From,To')
-  console.log(data)
+  .select('From,To').eq('From', user)
+  .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        console.log(data)
+        // setUsername(data.username)
+        // setWebsite(data.website)
+        // setAvatarUrl(data.avatar_url)
+      }
+  
 //   if(JSON.stringify(data.From)==journeyBeggin && JSON.stringify(data.To)==journeyOver){
 //     console.log(JSON.stringify(data) + "33");
 //   }
@@ -52,14 +95,14 @@ const flightui = () => {
 
 
 <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
-    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto mx-auto">
        
         <div class="relative bg-white rounded-lg shadow ">
             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 " data-modal-toggle="authentication-modal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Close modal</span>
             </button>
-            <div class="py-6 px-6  lg:px-8">
+            <div class="py-6 px-6  lg:px-8 ">
                 <h3 class="mb-4 text-2xl font-medium text-black ">FLIGHT</h3>
                 <form class="space-y-6" action="#">
                    
@@ -98,7 +141,7 @@ const flightui = () => {
                             <input type="text" name="password" id="password"  class=" text-gray-900 text-lg  focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-200 dark:border-gray-500 dark:placeholder-gray-400 dark:text-gray-800" value={` ${journeyOver.substring(0,3)} ${journeyOver} International AIRPORT...`} required=""/>
                         
                        </div>
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handlesearch}>Search</button>
                     {/* <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                         Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                     </div> */}
@@ -106,8 +149,81 @@ const flightui = () => {
             </div>
         </div>
     </div>
-</div> 
 
+{
+    flightsearch? 
+    (
+    <div class="p-10 ">
+    <div class="max-w-full  bg-white flex flex-col rounded overflow-hidden shadow-lg">
+        <div class="flex flex-row items-baseline flex-nowrap bg-gray-100 p-2">
+        {/* <svg viewBox="0 0 64 64" data-testid="tripDetails-bound-plane-icon" pointer-events="all" aria-hidden="true" class="mt-2 mr-1" role="presentation" style="fill: rgb(102, 102, 102); height: 0.9rem; width: 0.9rem;">
+            <path d="M43.389 38.269L29.222 61.34a1.152 1.152 0 01-1.064.615H20.99a1.219 1.219 0 01-1.007-.5 1.324 1.324 0 01-.2-1.149L26.2 38.27H11.7l-3.947 6.919a1.209 1.209 0 01-1.092.644H1.285a1.234 1.234 0 01-.895-.392l-.057-.056a1.427 1.427 0 01-.308-1.036L1.789 32 .025 19.656a1.182 1.182 0 01.281-1.009 1.356 1.356 0 01.951-.448l5.4-.027a1.227 1.227 0 01.9.391.85.85 0 01.2.252L11.7 25.73h14.5L19.792 3.7a1.324 1.324 0 01.2-1.149A1.219 1.219 0 0121 2.045h7.168a1.152 1.152 0 011.064.615l14.162 23.071h8.959a17.287 17.287 0 017.839 1.791Q63.777 29.315 64 32q-.224 2.685-3.807 4.478a17.282 17.282 0 01-7.84 1.793h-9.016z"></path>
+        </svg> */}
+        <h1 class="ml-2 uppercase font-bold text-gray-500">departure</h1>
+        <p class="ml-2 font-normal text-gray-500">{journeydate}</p>
+        </div>
+        <div class="mt-2 flex justify-start bg-white p-2">
+        <div class="flex mx-2 ml-6 h8 px-2 flex-row items-baseline rounded-full bg-gray-100 p-1">
+            {/* <svg viewBox="0 0 64 64" pointer-events="all" aria-hidden="true" class="etiIcon css-jbc4oa" role="presentation" style="fill: rgb(102, 102, 102); height: 12px; width: 12px;">
+            <path d="M43.389 38.269L29.222 61.34a1.152 1.152 0 01-1.064.615H20.99a1.219 1.219 0 01-1.007-.5 1.324 1.324 0 01-.2-1.149L26.2 38.27H11.7l-3.947 6.919a1.209 1.209 0 01-1.092.644H1.285a1.234 1.234 0 01-.895-.392l-.057-.056a1.427 1.427 0 01-.308-1.036L1.789 32 .025 19.656a1.182 1.182 0 01.281-1.009 1.356 1.356 0 01.951-.448l5.4-.027a1.227 1.227 0 01.9.391.85.85 0 01.2.252L11.7 25.73h14.5L19.792 3.7a1.324 1.324 0 01.2-1.149A1.219 1.219 0 0121 2.045h7.168a1.152 1.152 0 011.064.615l14.162 23.071h8.959a17.287 17.287 0 017.839 1.791Q63.777 29.315 64 32q-.224 2.685-3.807 4.478a17.282 17.282 0 01-7.84 1.793h-9.016z"></path>
+            </svg> */}
+            <p class="font-normal text-sm ml-1 text-gray-500">Economy</p>
+        </div>
+        </div>
+        <div class="mt-2 flex sm:flex-row mx-6 sm:justify-between flex-wrap ">
+        <div class="flex flex-row place-items-center p-2">
+            <div class="flex flex-col ml-2">
+            <p class="text-xl text-gray-500 font-bold">Indigo</p>
+            <p class="text-xs text-gray-500">QR1456</p>
+            <div class="text-xs text-gray-500">2*25kg</div>
+            </div>
+        </div>
+
+        <div class="flex flex-col p-2">
+            <p class="font-bold text-black">18:25</p>
+            <p class="text-gray-500 font-bold">{journeyBeggin}</p>
+            {/* <p class="text-gray-500">{` ${journeyBeggin.substring(0,3)} ${journeyBeggin} International AIRPORT`}</p> */}
+        </div>
+        <div class="flex flex-col p-2">
+            <p class=" text-gray-400">07 h 45 m</p>
+            <p className='text-yellow-400 font-bold'>----------</p>
+            <p className='text-gray-400'>Non stop</p>
+        </div>
+        <div class="flex flex-col flex-wrap p-2">
+            <p class="font-bold text-black">19:25</p>
+            <p class="text-gray-500 font-bold">{journeyOver}</p>
+            {/* <p class="text-gray-500">{` ${journeyOver.substring(0,3)} ${journeyOver} International AIRPORT...`}</p> */}
+        </div>
+        <div class="md:border-l-2 mx-6 md:border-dotted flex flex-row py-4 mr-6 flex-wrap">
+            <div class="text-sm mx-2 flex flex-col text-gray-400">
+            <p>Price</p>
+            <p class="font-bold">$6500</p>
+            <p class="text-xs text-gray-500">Price per adult</p>
+            </div>
+            <button class="w-32 h-11 rounded flex border-solid border text-white bg-green-800 mx-2 justify-center place-items-center" onClick={passengerdetail}>Book</button>
+        </div>
+        </div>
+        {/* <div class="mt-4 bg-gray-100 flex flex-row flex-wrap md:flex-nowrap justify-between items-baseline">
+        <div class="flex mx-6 py-4 flex-row flex-wrap">
+            <svg class="w-12 h-10 p-2 mx-2 self-center bg-gray-400 rounded-full fill-current text-white" viewBox="0 0 64 64" pointer-events="all" aria-hidden="true" role="presentation"><path d="M43.389 38.269L29.222 61.34a1.152 1.152 0 01-1.064.615H20.99a1.219 1.219 0 01-1.007-.5 1.324 1.324 0 01-.2-1.149L26.2 38.27H11.7l-3.947 6.919a1.209 1.209 0 01-1.092.644H1.285a1.234 1.234 0 01-.895-.392l-.057-.056a1.427 1.427 0 01-.308-1.036L1.789 32 .025 19.656a1.182 1.182 0 01.281-1.009 1.356 1.356 0 01.951-.448l5.4-.027a1.227 1.227 0 01.9.391.85.85 0 01.2.252L11.7 25.73h14.5L19.792 3.7a1.324 1.324 0 01.2-1.149A1.219 1.219 0 0121 2.045h7.168a1.152 1.152 0 011.064.615l14.162 23.071h8.959a17.287 17.287 0 017.839 1.791Q63.777 29.315 64 32q-.224 2.685-3.807 4.478a17.282 17.282 0 01-7.84 1.793h-9.016z"></path></svg>
+            <div class="text-sm mx-2 flex flex-col">
+            <p class="">Standard Ticket</p>
+            <p class="font-bold">$404.73</p>
+            <p class="text-xs text-gray-500">Price per adult</p>
+            </div>
+            <button class="w-32 h-11 rounded flex border-solid border bg-white mx-2 justify-center place-items-center"><div class="">Book</div></button>
+        </div>
+        
+        </div> */}
+    </div>
+    </div>
+    ) : (
+        <div></div>
+    )
+   
+    
+}
+</div> 
    </>
   )
 }
---
+++ pages/passenger.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+const passenger = () => {
+    const router=useRouter()
+    const travellerdetail=(e)=>{
+        e.preventDefault()
+        router.push("/boardingpass")
+    }
+  return (
+    <>
+
+<form>
+<div className='w-4/12 border bg-white rounded-lg py-5 m-auto my-80 px-4'>
+    <div className='font-bold text-black text-3xl '>Traveller Details</div>
+  <div class="relative z-0 mb-6  group my-5">
+      <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""/>
+      <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+  </div>
+  
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 mb-6 w-full group">
+        <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""/>
+        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+        <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""/>
+        <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+    </div>
+  </div>
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 mb-6 w-full group">
+        <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number </label>
+    </div>
+    <div class="relative z-0 mb-6 w-full group">
+        <input type="number"  name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required="" />
+        <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Age</label>
+    </div>
+    
+  </div>
+  <div className='flex flex-row justify-between my-3 text-center'>
+    <p className='text-black  font-bold'>Total Due</p>
+        <p className='text-black  font-bold'>6500</p>
+        <button className='bg-blue-500 rounded-xl px-7 py-2 '>Proceed</button>
+    </div>
+  <button type="submit" class="text-white bg-blue-700 items-center  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"  onClick={travellerdetail}>Submit</button>
+</div>
+</form>
+
+    </>
+  )
+}
+
+export default passenger
